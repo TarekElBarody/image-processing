805f716915d32e49b378166aabb0239af8fa72d7
Tarek El-Barody <tarek@tameemat.com>
2022-04-10 01:05:53 +0200

    Update Deploy

--- frontend/bin/deploy.sh
+++ frontend/bin/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-aws s3 cp --recursive ./build s3://image-processing-fronted-bucket/
+aws s3 cp --recursive --acl public-read ./build s3://image-processing-fronted-bucket/
 
 aws cloudfront create-invalidation --distribution-id E3SRADI6HYOK0D --paths "/*"
