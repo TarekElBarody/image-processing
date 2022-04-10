9af43b7d09dee52214a4a68ec57910a88ac42fa6
Tarek El-Barody <tarek@tameemat.com>
2022-04-10 10:35:49 +0200

    Update Deploy

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,32 @@
 import http from 'http';
 import https from 'https';
+import path from 'path';
+import fs from 'fs';
 import { app, port, sslPort, sslCert } from './lib/app';
 import { defaultAdmin } from './lib/functions/defaultAdmin';
 
 // check if is their no data on users it will insert default admin
 defaultAdmin();
+
+try {
+  const thumbDir = path.resolve('./images/thumb');
+  const fullDire = path.resolve('./images/full');
+  const tmpDire = path.resolve('./images/tmp');
+
+  if (!fs.existsSync(thumbDir)) {
+    fs.mkdirSync(thumbDir);
+  }
+
+  if (!fs.existsSync(fullDire)) {
+    fs.mkdirSync(fullDire);
+  }
+
+  if (!fs.existsSync(tmpDire)) {
+    fs.mkdirSync(tmpDire);
+  }
+} catch (error) {
+  console.log(error);
+}
 
 // Start HTTP Server
 const httpApp = http.createServer(app).listen(port, () => {
