3ff60df4f3a0e4b3494c2ed5262c448af3015ac1
Tarek El-Barody <tarek@tameemat.com>
2022-04-10 06:42:30 +0200

    Update Deploy

--- backend/bin/deploy.sh
+++ backend/bin/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-eb create Imageprocessingbackend-env
-eb deploy
+#eb create Imageprocessingbackend-env
+#eb deploy
 aws cloudfront create-invalidation --distribution-id EGJUPFQMKH5XJ --paths "/*"
