394f1bd97008ba425dfe9c02b9f29d3ff6b34c51
Tarek El-Barody <tarek@tameemat.com>
2022-04-17 06:53:52 +0200

    fix aws access id

--- frontend/bin/deploy.sh
+++ frontend/bin/deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #aws s3 cp --recursive --acl public-read ./build s3://image-processing-front-store/
-aws s3 sync  ./build s3://image-processing-front-store/ --delete
+aws s3 sync ./build s3://image-processing-front-store/ --delete
 aws cloudfront create-invalidation --distribution-id E195QY8FTHUTVJ --paths "/*"
